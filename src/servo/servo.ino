1f12fb2402cfeb3a72e418b086faf0cba6441681
Nico Ismaili <nico@ismaili.de>
2022-01-12 18:35:43 +0100

    add: piezo speaker activation

--- src/servo/servo.ino
+++ src/servo/servo.ino
@@ -4,9 +4,14 @@ Servo servo1;
 void setup()
 {
   servo1.attach(3);
-  servo1.write(180);
 }
 
 void loop()
 {
+  servo1.write(120);
+  tone(4, 100);
+  delay(1000);
+  servo1.write(30);
+  noTone(4);
+  delay(1000);
 }
